f41e69c2211d69f5f3b212295c75fd6ea67b530e
tara <tara@Mac.broadband>
2026-08-26 14:08:49 +0100

    manual param choice, one pair of stocks

--- newproj.py
+++ newproj.py
@@ -4,9 +4,10 @@ import itertools
 from statsmodels.tsa.stattools import coint
 from statsmodels.regression.rolling import RollingOLS
 from statsmodels.regression import linear_model
-import statsmodels as sm
+import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 import bocd
 
 tickers = ["AAPL", "MSFT", "GOOGL", "AMZN", "META", "TSLA", "V", "MA", "JPM", "BAC"]
@@ -15,143 +16,260 @@ end = "2023-01-01"
 split_date = "2019-12-31"
 data = yf.download(tickers, start=start, end=end)['Close'] 
 data = data.dropna()
-
+transaction_cost = 0.005
 
 training_data = data.loc[:split_date]
 trading_data = data.loc[split_date:].copy()
 combos = (itertools.combinations(tickers,2))
-pvalues = []
 
-# Before we can calculate a spread, we need to find two stocks that are actually tethered together. 
-# We do this using the Engle-Granger cointegration test.
+def engle_granger(combos, data):
+    pvalues = []
+    for a, b in combos:
+        series_a = data[a]
+        series_b = data[b]
+        _, pval, _ = coint(series_a, series_b)
+        pvalues.append((a,b,pval))
 
-for a, b in combos:
-    series_a = training_data[a]
-    series_b = training_data[b]
-    _, pval, _ = coint(series_a, series_b)
-    pvalues.append((a,b,pval))
+    pvaldf = pd.DataFrame(pvalues)
+    pvaldf.columns = ["Stock A", "Stock B", "p-value"]
+    pvaldf = pvaldf.sort_values("p-value", ascending=True)
+    return pvaldf
 
-pvaldf = pd.DataFrame(pvalues)
-pvaldf.columns = ["Stock A", "Stock B", "p-value"]
-pvaldf = pvaldf.sort_values("p-value", ascending=True)
-print(pvaldf)
+def calculate_spread(window, stock_a, stock_b, data):
+    ols = RollingOLS(data[stock_a], sm.add_constant(data[stock_b]), window)
+    model = ols.fit() 
+    alpha = model.params['const']
+    beta = model.params[stock_b]
+    spread = data[stock_a] - (beta * data[stock_b] + alpha )
+    return(spread, beta)
 
-# Now that we know which two stocks move together, we need to create the "elastic band" (the spread).
-# In your original code, you used Cov(A,B) / Var(B). This is flawed because it forces the spread to be centered 
-# at zero, completely ignoring the absolute price difference between the stocks (the intercept, alpha).
-# Instead, the professional standard is to run a Rolling Ordinary Least Squares (OLS) regression.
-# The mathematical relationship is: {Price}_A = \alpha + \beta \times \{Price}_B
-# Where: \beta (Slope / Hedge Ratio): How many shares of B we short for every 1 share of A.
-# \alpha (Intercept): The baseline price gap between them.
-# Once we know $\alpha$ and $\beta$, our stationary spread is simply the residual (the error of the regression):
+def calculate_halflife(spread):
+    change = spread.diff()
+    x = spread.shift()
+    x_c = sm.add_constant(x)
+    ols_spread = sm.OLS(change, x_c, missing='drop')
+    results = ols_spread.fit()
+    gradient = results.params.iloc[1]
+    if gradient >= 0:
+        print("Gradient is positive - series is not mean-reverting")
+        return np.nan
+    half_life = -np.log(2)/gradient #maybe add bounds in case it gets close to 0 as his will approach inf
+    return half_life
 
+def z_scores(spread, window):
+    spread_mean = spread.rolling(window=window).mean()
+    spread_std = spread.rolling(window=window).std() + 1e-8 #prevent division by zero
+    z_score = (spread - spread_mean)/spread_std
+    return z_score
 
-chosen_a, chosen_b = pvaldf.iloc[0, 0:2]
-#price_a = alpha + beta*price_b
-
-long_window_size = 60
-ols = RollingOLS(trading_data[chosen_a], sm.tools.add_constant(trading_data[chosen_b]), long_window_size)
-model = ols.fit() 
-alpha = model.params['const'] #NaNs for first 59 days as window is 60 days
-beta = model.params[chosen_b]
-
-# Spread = {Price}_A - (\beta \times \{Price}_B + \alpha)
-spread = trading_data[chosen_a] - (beta * trading_data[chosen_b] + alpha )
-plt.figure(figsize=(10,5))
-plt.axhline(0, linestyle="--")
-spread.plot(color="blue")
-plt.show()
-
-
-# We need to mathematically calculate exactly how many days it takes for our elastic band to snap halfway 
-# back to zero. This is called the Half-Life, derived from the Ornstein-Uhlenbeck (OU) process.
-# Here is the secret: to find the half-life, we just run another simple linear regression, but this time on the spread itself.
-# If a spread is mean-reverting, then the change in the spread today should depend on where 
-# the spread was yesterday. If yesterday's spread was huge, today's change should be a big negative move (snapping back).
-
-#calculate the daily change in spread 
-#regress against the previous days spread
-spread_clean = spread.dropna()
-print(spread_clean.head())
-change = np.array(spread_clean.diff(1).dropna())
-x = np.array(spread_clean.shift().dropna())
-
-ols_spread = linear_model.OLS(change, sm.tools.add_constant(x))
-results = ols_spread.fit()
-print(results.summary())
-
-#negative x1 shows mean reverting
-#half-life = -ln2/gradient 
-
-half_life = -np.log(2)/results.params[1]
-print(half_life)
-#half life 11 days
-
-short_window_size=(np.floor(half_life)).astype(int)
-
-spread_mean = spread.rolling(window=short_window_size).mean()
-spread_std = spread.rolling(window=short_window_size).std() + 1e-8 #prevent division by zero
-z_score = (spread - spread_mean)/spread_std
-trading_data["z_score"] = z_score
-print(z_score.tail())
-
-# We expect a structural break roughly once every 250 trading days (1 year)
-# We model the spread using a Student-T distribution (handles fat tails better than Gaussian)
-bayes_model = bocd.BayesianOnlineChangePointDetection(
-    bocd.ConstantHazard(250), #prior is one changepoint per year
-    bocd.StudentT(mu=0, kappa=1, alpha=1, beta=1) #financial market has fat tails
-)
-
-coint_p_vals = []
-padding = [np.nan] * 60 
-
-for i in range(60, len(trading_data)):
-    # Slice a rolling window that moves forward with 'i'
-    view = trading_data[[chosen_a, chosen_b]].iloc[i-60:i] 
+def bayesian_cp(data, z_score, stock_a, stock_b, long_window_size):
+    df = data.copy()
     
-    # Pass the two distinct columns into the cointegration test
-    _, pvalview, _ = coint(view[chosen_a], view[chosen_b])
-    coint_p_vals.append(pvalview)
+    bayes_model = bocd.BayesianOnlineChangePointDetection(
+        bocd.ConstantHazard(63), #prior is one cp per quarter
+        bocd.StudentT(mu=0, kappa=1, alpha=1, beta=1) 
+    )
 
-# Combine the padding and the values, then add to the dataframe
-trading_data["coint_p_vals"] = padding + coint_p_vals
+    coint_p_vals = [np.nan] * long_window_size 
+    arr_a = df[stock_a].values
+    arr_b = df[stock_b].values
 
-z_score_clean = z_score.dropna()
-cp_probs = []
-for value in z_score_clean:
-    bayes_model.update(value)
-    cp_probs.append(bayes_model.rt[0])
+    for i in range(long_window_size, len(df)):
+        view_a = arr_a[i-long_window_size : i]
+        view_b = arr_b[i-long_window_size : i]
+        
+        if np.isnan(view_a).any() or np.isnan(view_b).any():
+            coint_p_vals.append(np.nan)
+        else:
+            _, pvalview, _ = coint(view_a, view_b)
+            coint_p_vals.append(pvalview)
+            
+    df["coint_p_vals"] = coint_p_vals
+    z_score_clean = z_score.dropna()
+    cp_day = []
+    
+    for value in z_score_clean:
+        bayes_model.update(value)
+        cp_day.append(bayes_model.rt[0])
+        
+    df["cp_day"] = pd.Series(cp_day, index=z_score_clean.index)
+    return df
 
-padding = np.repeat(np.nan, (len(trading_data) - len(cp_probs)))
-padded_probs = np.append(padding,cp_probs)
-trading_data["cp_probs"] = padded_probs
-print(trading_data.tail())
+def generate_signals_and_returns(data, z_entry, z_exit=0.2, coint_thresh=0.05):
+    df = data.copy()
+    
+    brake_triggers = df["cp_day"].diff() < 0
+    df.loc[brake_triggers, "locked_down"] = True
+    #unlock if cointegration and brake isn't triggering today
+    valid_coint = (df["coint_p_vals"] < coint_thresh) & (~brake_triggers)
+    df.loc[valid_coint, "locked_down"] = False
 
-# 1. The Lock (Absolute Priority)
-brake_triggers = trading_data["cp_probs"].diff() < 0
-trading_data.loc[brake_triggers, "locked_down"] = True
+    df["locked_down"] = df["locked_down"].ffill().fillna(False)
+    conditions = [
+        (df["locked_down"] == True), 
+        (df["cp_day"].diff() < 0), 
+        (df["z_score"] < -z_entry), 
+        (df["z_score"] > z_entry), 
+        (abs(df["z_score"]) < z_exit)
+    ]
+    choices = [0, 0, 1, -1, 0]
+    
+    df["position"] = np.select(conditions, choices, default=np.nan)
+    df["position"] = df["position"].ffill().shift(1)
+    return df
 
-# 2. The Key (Only allowed to unlock if the brake is NOT triggering today)
-# We wrap both conditions in parentheses and use '&' for 'AND'
-valid_coint = (trading_data["coint_p_vals"] < 0.05) & (~brake_triggers)
-trading_data.loc[valid_coint, "locked_down"] = False
+def calculate_returns(stock_a, stock_b, beta, data=trading_data):
+    data["delta_A"] = data[stock_a].diff()
+    data["delta_B"] = data[stock_b].diff()
 
-# 3. The Flip-Flop
-trading_data["locked_down"] = trading_data["locked_down"].ffill().fillna(False)
+    data["beta_shifted"] = beta.shift(1)
+    data["gross_exposure"] = data[stock_a].shift(1) + (abs(data["beta_shifted"]) * data[stock_b].shift(1))
 
-conditions = [(trading_data["locked_down"]==True), (trading_data["cp_probs"].diff() <0), (trading_data["z_score"]< -2.0), (trading_data["z_score"]>2.0), (abs(trading_data["z_score"])<0.2)]
-choices = [0, 0, 1, -1, 0]
-trading_data["position"] = np.select(conditions, choices, default=np.nan)
-trading_data["position"] = trading_data["position"].ffill().shift(1)
+    # PnL = Delta Price_A - (beta * delta Price_B)
+    transaction_cost_dollars = transaction_cost * data["gross_exposure"] * abs(data["position"].diff())
+    data["spread_pnl"] = data["delta_A"] - (data["beta_shifted"] * data["delta_B"]) - transaction_cost_dollars
+    data["strategy_return"] = data["position"] * (data["spread_pnl"] / data["gross_exposure"])
+    data["cumulative_return"] = (1 + data["strategy_return"].fillna(0)).cumprod()
+    return data
 
-trading_data["price_ratio"] = trading_data[chosen_a]/data[chosen_b]
-trading_data["ratio_return"] = trading_data["price_ratio"].pct_change()
-trading_data["strategy_return"] = trading_data["position"] * trading_data["ratio_return"]
-trading_data["cumulative_return"] = (1 + trading_data["strategy_return"]).cumprod()
+def parameter_tuning(data, chosen_a, chosen_b):
+    ols_windows = range(20,40, 2) 
+    z_entries = np.arange(0.5, 2.0, 0.1) 
+    results = []
 
-total_ret = (trading_data["cumulative_return"].iloc[-1] - 1) * 100
-print(f"Final Strategy Return with BOCD Emergency Brake: {total_ret:.2f}%")
+    for win in ols_windows:
+        df_base = data.copy()
 
-changepoints = trading_data[trading_data["cp_probs"]==1.0]
-print(changepoints)
-trading_data.to_csv('my_pairs_trading_results.csv')
+        spread, beta = calculate_spread(win, chosen_a, chosen_b, df_base)
+        half_life = calculate_halflife(spread)
+        
+        if pd.isna(half_life) or half_life < 2:
+            continue
+            
+        short_w = int(np.floor(half_life))
+        df_base["z_score"] = z_scores(spread, short_w)
+        df_base = bayesian_cp(df_base, df_base["z_score"], chosen_a, chosen_b, win)
+        
+        for z in z_entries:
+            df = df_base.copy() 
+            
+            df = generate_signals_and_returns(df, z_entry=z)
+            df = calculate_returns(chosen_a, chosen_b, beta, data=df)
+            
+            total_ret = (df["cumulative_return"].iloc[-1] - 1) * 100
+            daily_vol = df['strategy_return'].std()
+            sharpe = (df['strategy_return'].mean() / daily_vol) * np.sqrt(252) if daily_vol > 0 else 0
+            rolling_max = df["cumulative_return"].cummax()
+            drawdown = (df["cumulative_return"] - rolling_max) / rolling_max
+            max_dd = abs(drawdown.min())
+            calmar = (total_ret / 100) / (max_dd + 1e-8)
+            results.append({
+                "Window": win, 
+                "Z-Entry": round(z, 2), 
+                "Return (%)": total_ret, 
+                "Sharpe": sharpe,
+                "Calmar": calmar
+            })
+
+    results_df = pd.DataFrame(results)
+    return results_df
+
+def plot_optimization_surface(df, metric="Calmar"):
+    pivot_table = df.pivot(index="Window", columns="Z-Entry", values=metric)
+    
+    plt.figure(figsize=(14, 10))
+    sns.heatmap(
+        pivot_table, 
+        annot=True,          
+        cmap="coolwarm",     
+        center=0,            
+        fmt=".2f",           
+        cbar_kws={'label': metric}
+    )
+    
+    plt.title(f"Parameter Surface based on {metric}")
+    plt.xlabel("Z-Score")
+    plt.ylabel("Window")
+
+    plt.show()
+
+def print_strategy_summary(df):
+    total_return = (df["cumulative_return"].iloc[-1] - 1) * 100
+    
+    daily_vol = df['strategy_return'].std()
+    sharpe = (df['strategy_return'].mean() / daily_vol) * np.sqrt(252) if daily_vol > 0 else 0
+
+    rolling_max = df["cumulative_return"].cummax()
+    drawdown = (df["cumulative_return"] - rolling_max) / rolling_max
+    max_drawdown = drawdown.min() * 100
+    
+    total_trades = (df["position"].diff().fillna(0) != 0).sum()
+    days_locked_down = (df["locked_down"] == True).sum()
+    total_changepoints = (df["cp_day"].diff() < 0).sum()
+    
+    print("\n" + "="*40)
+    print("      STRATEGY PERFORMANCE SUMMARY")
+    print("="*40)
+    print(f"Total Cumulative Return: {total_return:>8.2f}%")
+    print(f"Sharpe Ratio:            {sharpe:>8.2f}")
+    print(f"Maximum Drawdown:        {max_drawdown:>8.2f}%")
+    print(f"Total Position Changes:  {total_trades:>8}")
+    print(f"Days Locked Down (Brake):{days_locked_down:>8}")
+    print(f"Number of Changepoints Detected:{total_changepoints:>8}")
+    print("="*40 + "\n")
+
+def run_strategy(data, chosen_a, chosen_b, long_window, z_entry):
+    df = data.copy()
+    spread, beta = calculate_spread(long_window, stock_a=chosen_a, stock_b=chosen_b, data=df)
+    half_life = calculate_halflife(spread)
+    if pd.isna(half_life) or half_life < 2:
+        return None
+    short_window_size=(np.floor(half_life)).astype(int)
+    print("window is", short_window_size)
+    df["z_score"] = z_scores(spread=spread, window=short_window_size)
+    print("z scores between", df["z_score"].min(), " and ", df["z_score"].max())
+    df = bayesian_cp(df, df["z_score"], chosen_a, chosen_b, long_window)
+    df = generate_signals_and_returns(df, z_entry=z_entry)
+    df = calculate_returns(chosen_a, chosen_b, beta, data=df)
+    return df
+####
+
+def execute(combos, training_data, trading_data):
+    #find pair to trade
+    pvaldf = engle_granger(combos=combos, data=training_data)
+    chosen_a, chosen_b = pvaldf.iloc[0, 0:2]
+    print(f"Selected Pair: {chosen_a} , {chosen_b}")
+
+    #tune params based on this pair
+    print("Tuning parameters on training data")
+    tuning_results = parameter_tuning(training_data, chosen_a, chosen_b)
+
+    if tuning_results.empty:
+        print("Parameter tuning failed.")
+        return None
+
+    plot_optimization_surface(tuning_results, metric="Calmar")
+    try:
+        best_window = int(input("Enter the chosen window: "))
+        best_z = float(input("Enter the chosen z score threshold: "))
+    except ValueError:
+        print("Invalid input")
+        return None
+    
+    out_of_sample_df = run_strategy(trading_data, chosen_a, chosen_b, best_window, best_z)
+    
+    if out_of_sample_df is None:
+        print("Failed")
+        return None
+        
+    out_of_sample_df["cumulative_return"].plot(
+        figsize=(10, 5), 
+        title=f"Return: {chosen_a} & {chosen_b}"
+    )
+    plt.show()
+    print_strategy_summary(out_of_sample_df)
+    
+    return out_of_sample_df
+
+execute(combos, training_data, trading_data)
+
